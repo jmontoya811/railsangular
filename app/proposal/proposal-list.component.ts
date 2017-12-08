a3816145b716725a63a0cbec2a0e9ecf69453e51
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-12-08 15:03:28 -0700

    added proposal list data

--- app/proposal/proposal-list.component.ts
+++ app/proposal/proposal-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
 	moduleId: module.id,
 	selector: 'proposal-list',
 	templateUrl: 'proposal-list.component.html'
 })
-export class ProposalListComponent {}
+export class ProposalListComponent {
+	proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://myspecialportfolio.herokuapp.com', 'Ruby on Rails', 150, 120, 15, 'development.btga@gmail.com')
+	proposalTwo: Proposal = new Proposal(99, 'The Big Company', 'http://myspecialportfolio.herokuapp.com', 'Ruby on Rails', 150, 120, 15, 'development.btga@gmail.com')
+	proposalThree: Proposal = new Proposal(300, 'TYX Company', 'http://myspecialportfolio.herokuapp.com', 'Ruby on Rails', 150, 120, 15, 'development.btga@gmail.com')
+
+	proposals: Proposal[] = [
+		this.proposalOne,
+		this.proposalTwo,
+		this.proposalThree
+  ]
+}
